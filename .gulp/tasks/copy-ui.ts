86b8563ac854e18523987d91aa0be0f2b6286b0e
LasaleFamine <info@godev.space>
2018-06-23 21:03:13 +0200

    WIP (webview): added gulp command for copying ui files

---
+++ .gulp/tasks/copy-ui.ts
@@ -0,0 +1,24 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as gulp from 'gulp';
+
+import {PATHS} from '../../extensions/consts/paths';
+import { ensureDir } from '../../extensions/helpers/fs';
+
+/**
+ * For each ThemeIconVariant create a Material-Theme-Icons-{variant}.json
+ * depends on default Material-Theme-Icons.json
+ */
+export default gulp.task('build:copy-ui', callback => {
+  try {
+      ensureDir(path.resolve(PATHS.UI));
+      fs.copyFileSync(
+        path.join(PATHS.SRC, 'webviews', 'ui', 'settings.html'),
+        path.join(PATHS.UI, 'settings.html')
+      );
+  } catch (error) {
+    return callback(error);
+  }
+
+  callback();
+});
