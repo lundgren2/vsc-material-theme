56712cd318433e6f7775444a9ee6a33fd569c58b
LasaleFamine <info@godev.space>
2018-06-24 20:52:28 +0200

    chore: added new task on task explorer and small fix copy ui task

--- .gulp/tasks/copy-ui.ts
+++ .gulp/tasks/copy-ui.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as gulp from 'gulp';
 
 import {PATHS} from '../../extensions/consts/paths';
-import { ensureDir } from '../../extensions/helpers/fs';
+import {ensureDir} from '../../extensions/helpers/fs';
 
 /**
  * For each ThemeIconVariant create a Material-Theme-Icons-{variant}.json
@@ -13,8 +13,12 @@ export default gulp.task('build:copy-ui', callback => {
   try {
       ensureDir(path.resolve(PATHS.UI));
       fs.copyFileSync(
-        path.join(PATHS.SRC, 'webviews', 'ui', 'settings.html'),
+        path.join(PATHS.SRC, 'webviews', 'ui', 'settings', 'settings.html'),
         path.join(PATHS.UI, 'settings.html')
+      );
+      fs.copyFileSync(
+        path.join(PATHS.SRC, 'webviews', 'ui', 'settings', 'style.css'),
+        path.join(PATHS.UI, 'settings.css')
       );
   } catch (error) {
     return callback(error);
